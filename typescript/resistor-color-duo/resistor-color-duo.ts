26b55be42628921043078ed93d6713aaa1d48a44
lucas.s.passos <lucas.s.passos@stf.jus.br>
2022-10-23 06:23:41 -0300

    solução dos exercicios two-fer e resistor-color-duo

---
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,94 @@
+export function decodedValue(colornames: string[]) {
+
+  const colorsnumber = []
+
+  switch(colornames[0]){
+    case 'black':
+    colorsnumber[0] = 0
+      break;
+  
+    case 'brown':
+    colorsnumber[0] = 1
+      break;
+  
+    case 'red':
+    colorsnumber[0] = 2
+      break;
+    
+    case 'orange':
+    colorsnumber[0] = 3
+      break;
+
+    case 'yellow':
+    colorsnumber[0] = 4
+      break;
+
+    case 'green':
+    colorsnumber[0] = 5
+      break;
+
+    case 'blue':
+    colorsnumber[0] = 6
+      break;
+
+    case 'violet':
+    colorsnumber[0] = 7
+      break; 
+
+    case 'grey':
+    colorsnumber[0] = 8
+      break;
+
+    case 'white':
+    colorsnumber[0] = 9
+      break; 
+
+  }
+
+  switch(colornames[1]){
+    case 'black':
+    colorsnumber[1] = 0
+      break;
+  
+    case 'brown':
+    colorsnumber[1] = 1
+      break;
+  
+    case 'red':
+    colorsnumber[1] = 2
+      break;
+    
+    case 'orange':
+    colorsnumber[1] = 3
+      break;
+
+    case 'yellow':
+    colorsnumber[1] = 4
+      break;
+
+    case 'green':
+    colorsnumber[1] = 5
+      break;
+
+    case 'blue':
+    colorsnumber[1] = 6
+      break;
+
+    case 'violet':
+    colorsnumber[1] = 7
+      break; 
+
+    case 'grey':
+    colorsnumber[1] = 8
+      break;
+
+    case 'white':
+    colorsnumber[1] = 9
+      break; 
+
+  }
+  
+  colorsnumber[0] = colorsnumber[0]*10
+  return colorsnumber[0]+colorsnumber[1]
+    
+}
